d4a0da0f6227cb9cf4f03b944de817610729e26c
soundake <soundake@gmail.com>
2013-10-30 22:40:43 +0100

    MultiUploadControl
    Nette UploadControl Form Extensiion for manipulation with more files

---
+++ src/MultiUploadControl.php
@@ -0,0 +1,83 @@
+<?php
+namespace soundake\utils;
+
+use \Nette,
+	\Nette\Http;
+
+class MultiUploadControl extends \Nette\Forms\Controls\UploadControl
+{
+
+	public function getHtmlName()
+	{
+		return parent::getHtmlName() . '[]';
+	}
+
+	public function getControl()
+	{
+		return parent::getControl()->multiple(TRUE);
+	}
+
+	public function setValue($value)
+	{
+		if (!is_null($value)) {
+			foreach ($value as $single) {
+				if (is_array($single)) {
+					$this->value[] = new Http\FileUpload($single);
+				} elseif ($single instanceof Http\FileUpload) {
+					$this->value[] = $single;
+				} else {
+					$this->value[] = new Http\FileUpload(NULL);
+				}
+			}
+		}
+		return $this;
+	}
+
+	public function isFilled()
+	{
+		return $this->value[0] instanceof Http\FileUpload && $this->value[0]->isOK();
+	}
+
+	public static function validateFileSize(UploadControl $control, $limit)
+	{
+		$files = $control->getValue();
+		foreach ($files as $file) {
+			if ($file instanceof Http\FileUpload && $file->getSize() <= $limit) {
+			} else
+				return false;
+		}
+
+		return true;
+	}
+
+	public static function validateMimeType(UploadControl $control, $mimeType)
+	{
+		$files = $control->getValue();
+		foreach ($files as $file) {
+			if ($file instanceof Http\FileUpload) {
+				$type = strtolower($file->getContentType());
+				$mimeTypes = is_array($mimeType) ? $mimeType : explode(',', $mimeType);
+				if (!in_array($type, $mimeTypes, TRUE)) {
+					return FALSE;
+				}
+				if (!in_array(preg_replace('#/.*#', '/*', $type), $mimeTypes, TRUE)) {
+					return FALSE;
+				}
+			}
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+	public static function validateImage(UploadControl $control)
+	{
+		$files = $control->getValue();
+		foreach ($files as $file) {
+			if ($file instanceof Http\FileUpload && $file->isImage()) {
+			} else
+				return false;
+		}
+
+		return true;
+	}
+}
